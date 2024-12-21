191553f4026e046534a9f112483e83ce685f64a2
naghost-dev <miguelangel.hernandez12@outlook.com>
2024-12-21 11:24:58 +0100

    update routes to continue thread_id

--- src/app/routes/__init__.py
+++ src/app/routes/__init__.py
@@ -1,7 +1,9 @@
+import uuid
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
 from langgraph.graph.state import CompiledStateGraph
+from pydantic import BaseModel
 
 from app.dependencies import get_workflow
 
@@ -9,12 +11,30 @@ router = APIRouter(prefix="/agents",
                    dependencies=[])
 
 
-@router.post("/invoke", tags=["agents"])
-def invoke(prompt: str, workflow: Annotated[CompiledStateGraph, Depends(get_workflow)]):
-    config={
+class InvokeResponseModel(BaseModel):
+    configuration: dict
+    response: list
+
+
+def get_config(thread_id: str=None):
+    if thread_id is None:
+        thread_id=str(uuid.uuid4())
+    return {
         "configurable": {
-            "thread_id": 1
+            "thread_id": thread_id
         }
     }
-    return workflow.invoke({"messages": [{"role": "user", "content": prompt}]}, config, stream_mode="updates")
 
+
+@router.post("/invoke", tags=["agents"])
+def invoke(prompt: str, workflow: Annotated[CompiledStateGraph, Depends(get_workflow)]) -> InvokeResponseModel:
+    config = get_config()
+    response = workflow.invoke({"messages": [{"role": "user", "content": prompt}]}, config, stream_mode="updates")
+    return InvokeResponseModel(response=response, configuration=config)
+
+
+@router.post("/invoke/{thread_id}", tags=["agents"])
+def invoke(prompt: str, thread_id: str, workflow: Annotated[CompiledStateGraph, Depends(get_workflow)]) -> InvokeResponseModel:
+    config = get_config(thread_id)
+    response = workflow.invoke({"messages": [{"role": "user", "content": prompt}]}, config, stream_mode="updates")
+    return InvokeResponseModel(response=response, configuration=config)
